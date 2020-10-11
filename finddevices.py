a4d03f5559b56df4a808f9d1ae6fe53c62032734
HeroBrine1st Erquilenne <herobrine1st@pm.me>
2020-10-11 21:41:47 +0300

    Ганс, неси огнемет (refactoring needed)

--- finddevices.py
+++ finddevices.py
@@ -1,5 +1,12 @@
 import evdev
+from evdev.ecodes import EV_ABS
 
 for d in evdev.list_devices():
   device = evdev.InputDevice(d)
-  print(device)
+  a = device.capabilities()
+  tab = False
+  if EV_ABS in a:
+    if len(a[EV_ABS]) >= 3:
+      tab = True
+  print(device.path, device.name, tab)
+
--- tabletdriver.py
+++ tabletdriver.py
@@ -1,19 +1,93 @@
+import sys
+
 from evdev import UInput, InputDevice, AbsInfo
 from evdev.ecodes import *
+import os
+import json
+import evdev
 import mouseulits
 
-path = "/dev/input/event11"
+path = ""
 # top bottom left right
-wacom_area = [16000, 22094, 1714, 5140]
+# wacom_area = [16000, 22094, 1714, 5140]
+wacom_area = [0, 32000, 0, 18000]
 
 
 wacom_size = [wacom_area[1] - wacom_area[0], wacom_area[3] - wacom_area[2]]
+device = None
+key_mapping = {
+    BTN_TOUCH: BTN_TOUCH,
+    BTN_STYLUS: BTN_STYLUS2,
+    BTN_STYLUS2: BTN_STYLUS,
+}
 
-device = InputDevice(path)
+def find_tablet():
+    for dev in evdev.list_devices():
+        dev = InputDevice(dev)
+        a = dev.capabilities()
+        if EV_ABS in a:
+            if len(a[EV_ABS]) >= 3:
+                return dev
+    return None
+
+def load_tablet(query):
+    try:
+        dev = InputDevice(query["path"])
+    except FileNotFoundError:
+        dev = None
+        for dev in evdev.list_devices():
+            dev = InputDevice(dev)
+            if dev.info.vendor == query["vendor"] and dev.info.product == query["product"]:
+                break
+        if dev is None:
+            return None
+    cap = dev.capabilities()
+    if EV_ABS in cap and len(cap[EV_ABS]) >= 3:
+        return dev
+    return None
+
+def save_config():
+    config = {
+        "device": {
+            "path": device.path,
+            "product": device.info.product,
+            "vendor": device.info.vendor,
+        },
+        "area": wacom_area,
+    }
+    with open("profile.json", "w") as f:
+        json.dump(config, f)
+
+
+def init():
+    global device
+    global wacom_area
+    global wacom_size
+    if os.path.exists("profile.json"):
+        with open("profile.json") as f:
+            config = json.load(f)
+        device = load_tablet(config["device"])
+        if device is None:
+            print("Couldn't load tablet from config.", file=sys.stderr)
+            exit(0)
+        wacom_area = config["area"]
+        wacom_size = [wacom_area[1] - wacom_area[0], wacom_area[3] - wacom_area[2]]
+    else:
+        print("No config found.")
+        device = find_tablet()
+        if device is None:
+            print("No tablets connected.", file=sys.stderr)
+            exit(0)
+        print("Found tablet %s (Vendor %s, Product %s) on %s" % (device.name, device.info.vendor, device.info.product, device.path))
+        print("Using default config")
+        save_config()
+        print("Config saved")
+
+
+
 
 def main():
-
-    print("Running tabletdriver on device %s (%s)" % (path, device.name))
+    print("Running tabletdriver on device %s (%s)" % (device.path, device.name))
     cap = device.capabilities()
     display = mouseulits.size()
     uinput = UInput({
@@ -51,6 +125,7 @@ def main():
 
 if __name__ == '__main__':
     try:
+        init()
         with device.grab_context():
             main()
     except KeyboardInterrupt:
