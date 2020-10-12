f2762bf101e8f52aac04217f2075316923d43506
HeroBrine1st Erquilenne <herobrine1st@pm.me>
2020-10-12 16:46:59 +0300

    Рефакторинг

--- README.md
+++ README.md
@@ -3,10 +3,13 @@ XP-PEN Star G640 driver written in python.
 
 Potentially supports all (at least more than one) graphic tablets, I didn't test it (because I have only one).
 
-This driver providing only customizing tablet area. If your distro doesn't have integrated tablet drivers this driver won't work.
-Maybe I will create option for such distros so that driver will send event directly to xorg without virtual device, but pressure sensivity won't available.   
+This driver is a layer between xorg and device, so if your distro doesn't have integrated drivers, this driver won't work (no chance, only if you use very old linux kernel)   
 
 Work in progress.
+
+# Features
+
+* Custom tablet area
 
 # Requirements
 
@@ -23,15 +26,20 @@ Disclaimer: I'm **not** responsible for any damages. You're doing this at your o
 2. Reboot system or run ``sudo udevadm control --reload-rules && sudo udevadm trigger``
 3. Install all requirements from file ``requirements.txt``
     * Python 3.6 recommended
-4. Run file ``finddevices.py`` and get your tablet's path 
-    * For me it was ``device /dev/input/event11, name "XP-PEN STAR G640 Pen", phys "usb-XXXX:XX:XX.X-X.X/input1"`` and path was ``/dev/input/event11``
-5. Open file ``tabletdriver.py`` and insert path. Edit settings if needed.
-6. Run ``tabletdriver.py``
+4. Run file ``finddevices.py`` and get your tablet's path
+    * ``True`` at the end of line explains device is tablet
+    * For me it was ``/dev/input/event15 XP-PEN STAR G640 Pen True`` and path was ``/dev/input/event15``
+5. Run ``daemon.py`` with arguments
+    1. Tablet's path
+    2. Tablet area start X
+    3. Area start Y
+    4. Area end X
+    5. Area end Y
+    * Command example: ``python daemon.py /dev/input/event15 0 0 32000 18000``
     * I didn't create autostart so you need to run driver manually.
     
 # Future plans
 
 * Auto-searching for tablets
-* Option for not use virtual tablet (send event directly to xorg)
 * GUI
 * Autostart and daemon process
---
+++ daemon.py
@@ -0,0 +1,51 @@
+import sys
+import mouseulits
+import evdev.ecodes
+from evdev import InputDevice, UInput, AbsInfo
+from evdev.ecodes import *
+
+
+display = mouseulits.size()
+area = tuple(map(int, sys.argv[2:6]))
+area_size = [area[2] - area[0], area[3] - area[1]]
+device = InputDevice(sys.argv[1])
+print("Using device %s" % device.name)
+cap = device.capabilities()
+print("Max X: %s\nMax Y: %s\n%s button(s)" %
+      (cap[3][0][1].max, cap[3][1][1].max, len(list(filter(lambda key: key != BTN_TOOL_PEN and key != BTN_TOUCH, cap[EV_KEY])))))
+if BTN_TOOL_PEN in cap[EV_KEY]:
+    print("Hover available")
+if BTN_TOUCH in cap[EV_KEY]:
+    print("Has BTN_TOUCH feature - %s pressure steps" % (cap[3][2][1].max + 1))
+
+virtual_device = UInput({
+    EV_KEY: cap[EV_KEY],
+    EV_ABS: [(0, AbsInfo(value=0, min=0, max=display[0], fuzz=0, flat=0, resolution=1)),
+             (1, AbsInfo(value=0, min=0, max=display[1], fuzz=0, flat=0, resolution=1)),
+             (24, AbsInfo(value=0, min=0, max=cap[EV_ABS][2][1].max, fuzz=0, flat=0, resolution=0))]
+})
+
+with device.grab_context():
+    while True:
+        event = device.read_one()
+        if event is None:
+            continue
+        if event.type == EV_ABS:
+            if event.code == ABS_X:
+                x = event.value
+                if x > area[2]:
+                    x = area[2]
+                x = (x - area[0]) * display[0] // area_size[0]
+                virtual_device.write(EV_ABS, ABS_X, x)
+            elif event.code == ABS_Y:
+                y = event.value
+                if y > area[3]:
+                    y = area[3]
+                y = (y - area[1]) * display[1] // area_size[1]
+                virtual_device.write(EV_ABS, ABS_Y, y)
+            elif event.code == ABS_PRESSURE:
+                virtual_device.write(EV_ABS, ABS_PRESSURE, event.value)
+            virtual_device.syn()
+        elif event.type == EV_KEY:
+            virtual_device.write(EV_KEY, event.code, event.value)
+            virtual_device.syn()
