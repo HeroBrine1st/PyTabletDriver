b4fff9e575a42892aa1ab500361395e0d3d40c52
HeroBrine1st Erquilenne <herobrine1st@pm.me>
2020-11-08 22:56:39 +0300

    [WIP] Переписываем конфигурацию драйвера; Ожидание подключения устройства после отключения

--- daemon.py
+++ daemon.py
@@ -1,54 +1,136 @@
 import sys
+from typing import Union
+
 import Xlib.display
 from evdev import InputDevice, UInput, AbsInfo
 from evdev.ecodes import *
+from pyudev import Monitor, Context
+from errno import *
+
 
 
 display = Xlib.display.Display()
-display_size = (display.screen().width_in_pixels, display.screen().height_in_pixels)
-area = tuple(map(int, sys.argv[2:6]))
-area_size = [area[2] - area[0], area[3] - area[1]]
-device = InputDevice(sys.argv[1])
-print("Using device %s" % device.name)
-cap = device.capabilities()
-print("Max X: %s\nMax Y: %s\n%s button(s)" %
-      (cap[3][0][1].max, cap[3][1][1].max, len(list(filter(lambda key: key != BTN_TOOL_PEN and key != BTN_TOUCH, cap[EV_KEY])))))
-if BTN_TOOL_PEN in cap[EV_KEY]:
-    print("Hover available")
-if BTN_TOUCH in cap[EV_KEY]:
-    print("Has BTN_TOUCH feature - %s pressure steps" % (cap[3][2][1].max + 1))
 
-virtual_device = UInput({
-    EV_KEY: cap[EV_KEY],
-    EV_ABS: [(0, AbsInfo(value=0, min=0, max=display_size[0], fuzz=0, flat=0, resolution=1)),
-             (1, AbsInfo(value=0, min=0, max=display_size[1], fuzz=0, flat=0, resolution=1)),
-             (24, AbsInfo(value=0, min=0, max=cap[EV_ABS][2][1].max, fuzz=0, flat=0, resolution=0))]
-})
+# CONFIGURATION VARIABLES #
+
+display_size = (display.screen().width_in_pixels, display.screen().height_in_pixels)
+display_pos = (0, 0)
+area = (0, 0, 1, 1)
+area_size = (area[2] - area[0], area[3] - area[1])
+
+# ======================= #
+
+device: Union[InputDevice, None] = None
+virtual_device: Union[UInput, None] = None
+
+device_searcher_info = {"vendor": 0, "product": 0}
+
+context = Context()
+monitor = Monitor.from_netlink(context)
+monitor.filter_by(subsystem='input')
+monitor.start()
+monitor.poll()
+
+
+
+def update_device(new_device):
+    global device
+    global virtual_device
+    new_device.grab() # Prevent xorg from overriding cursor pos
+    cap = new_device.capabilities()
+    virtual_device = UInput({
+        EV_KEY: cap[EV_KEY],
+        EV_ABS: [(0, AbsInfo(value=0, min=0, max=display_size[0], fuzz=0, flat=0, resolution=1)),
+                 (1, AbsInfo(value=0, min=0, max=display_size[1], fuzz=0, flat=0, resolution=1)),
+                 (24, AbsInfo(value=0, min=0, max=cap[EV_ABS][2][1].max, fuzz=0, flat=0, resolution=0))]
+    })
+    print("Using device %s" % new_device.name)
+    cap = new_device.capabilities()
+    print("Max X: %s\nMax Y: %s\n%s button(s)" %
+          (cap[3][0][1].max, cap[3][1][1].max,
+           len(list(filter(lambda key: key != BTN_TOOL_PEN and key != BTN_TOUCH, cap[EV_KEY])))))
+    if BTN_TOOL_PEN in cap[EV_KEY]:
+        print("Hover available")
+    if BTN_TOUCH in cap[EV_KEY]:
+        print("Has BTN_TOUCH feature - %s pressure steps" % (cap[3][2][1].max + 1))
+    device = new_device
+
+def process_command(cmd: str):
+    args = cmd.split(" ")
+    cmd = args.pop(0)
+    if len(args) == 0:
+        print("Invalid command")
+        return
+    if cmd == "AREA":
+        if len(args) != 4:
+            print("Invalid area")
+            return
+        global area
+        global area_size
+        try:
+            area_temp = tuple(map(int, cmd))
+            area_size_temp = (area_temp[2] - area_temp[0], area_temp[3] - area_temp[1])
+            if area_size[0] > 0 and area_size[1] > 0:
+                area = area_temp
+                area_size = area_size_temp
+            else:
+                print("Invalid area")
+        except ValueError:
+            print("Invalid area")
+    elif cmd == "DEVICE":
+        global device
+        if device is not None:
+            device.close()
+        path = args.pop(0)
+        new_device = InputDevice(path)
+        if EV_ABS not in new_device.capabilities() or len(new_device.capabilities()[EV_ABS]) < 3 \
+                or new_device.capabilities()[EV_ABS][2][0] != ABS_PRESSURE:
+            print("Invalid device")
+            return
+        update_device(new_device)
 
 def main():
+    global device
+    global virtual_device
+    device: Union[InputDevice, None]
+    virtual_device: Union[UInput, None]
     while True:
-        event = device.read_one()
-        if event is None:
-            continue
-        if event.type == EV_ABS:
-            if event.code == ABS_X:
-                x = event.value
-                if x > area[2]:
-                    x = area[2]
-                x = (x - area[0]) * display_size[0] // area_size[0]
-                virtual_device.write(EV_ABS, ABS_X, x)
-            elif event.code == ABS_Y:
-                y = event.value
-                if y > area[3]:
-                    y = area[3]
-                y = (y - area[1]) * display_size[1] // area_size[1]
-                virtual_device.write(EV_ABS, ABS_Y, y)
-            elif event.code == ABS_PRESSURE:
-                virtual_device.write(EV_ABS, ABS_PRESSURE, event.value)
-            virtual_device.syn()
-        elif event.type == EV_KEY:
-            virtual_device.write(EV_KEY, event.code, event.value)
-            virtual_device.syn()
+        if device and virtual_device:
+            try:  # Faster than monitor.poll(timeout=0) and checking udev events
+                event = device.read_one()
+                if event is None:
+                    continue
+                if event.type == EV_ABS:
+                    if event.code == ABS_X:
+                        x = event.value
+                        if x > area[2]:
+                            x = area[2]
+                        x = (x - area[0]) * display_size[0] // area_size[0]
+                        virtual_device.write(EV_ABS, ABS_X, x)
+                    elif event.code == ABS_Y:
+                        y = event.value
+                        if y > area[3]:
+                            y = area[3]
+                        y = (y - area[1]) * display_size[1] // area_size[1]
+                        virtual_device.write(EV_ABS, ABS_Y, y)
+                    elif event.code == ABS_PRESSURE:
+                        virtual_device.write(EV_ABS, ABS_PRESSURE, event.value)
+                    virtual_device.syn()
+                elif event.type == EV_KEY:
+                    virtual_device.write(EV_KEY, event.code, event.value)
+                    virtual_device.syn()
+            except OSError as e:
+                if e.args[0] == ENODEV:
+                    device_searcher_info["vendor"] = device.info.vendor
+                    device_searcher_info["product"] = device.info.product
+                    device = None
+                    virtual_device = None
+        else: # Wait for device connect
+            d = monitor.poll(timeout=0)
+            if d.device_node:
+                aspt = InputDevice(d.device_node)  # aspirant
+                if aspt.info.vendor == device_searcher_info["vendor"] and aspt.info.product == device_searcher_info["product"]:
+                    update_device(aspt)
 
 
 if __name__ == '__main__':
